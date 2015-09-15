6b429743012845e6d3cb54447414cb1328b00d54
vishal <vishal@qburst.com>
2015-09-15 18:25:47 +0530

    added jquery

--- shopping cart/js/script.js
+++ shopping cart/js/script.js
@@ -4,36 +4,36 @@ var prods = [], pname, name_value, pprice, price_value, pavail, avail_value, tot
 
 function display(n) {
     "use strict";
-    document.getElementById("disp").innerHTML = "";
-    var br1 = document.createElement("br"), br2 = document.createElement("br"), br3 = document.createElement("br"), head = document.createElement("h3");
-    total_value = document.createElement("span");
-    document.getElementById("disp").appendChild(br1);
-    document.getElementById("disp").appendChild(br2);
-    head.innerHTML = "Product Details";
-    document.getElementById("disp").appendChild(head);
-    pname = document.createElement("span");
-    name_value = document.createElement("span");
-    pprice = document.createElement("span");
-    price_value = document.createElement("span");
-    pavail = document.createElement("span");
-    avail_value = document.createElement("span");
-    pname.innerHTML = "Product name:  ";
-    name_value.innerHTML = prods[n].name;
-    pprice.innerHTML = "Product price:  ";
-    price_value.innerHTML = prods[n].price;
-    pavail.innerHTML = "Product availability:  ";
-    avail_value.innerHTML = prods[n].availability;
-    total_value.innerHTML = "Total price: " + tot;
-    document.getElementById("disp").appendChild(pname);
-    document.getElementById("disp").appendChild(name_value);
-    document.getElementById("disp").appendChild(br1);
-    document.getElementById("disp").appendChild(pprice);
-    document.getElementById("disp").appendChild(price_value);
-    document.getElementById("disp").appendChild(br2);
-    document.getElementById("disp").appendChild(pavail);
-    document.getElementById("disp").appendChild(avail_value);
-    document.getElementById("disp").appendChild(br3);
-    document.getElementById("disp").appendChild(total_value);
+    $("#disp").empty();
+    var br1 = $("<br>"), br2 = $("<br>"), br3 = $("<br>"), head = $("<h3>"), show = $("#disp");
+    total_value = $("<span></span>");
+    show.append(br1);
+    show.append(br2);
+    head.text("Product Details");
+    show.append(head);
+    pname = $("<span></span>");
+    name_value = $("<span></span>");
+    pprice = $("<span></span>");
+    price_value = $("<span></span>");
+    pavail = $("<span></span>");
+    avail_value = $("<span></span>");
+    pname.text("Product name:  ");
+    name_value.text(prods[n].name);
+    pprice.text("Product price:  ");
+    price_value.text(prods[n].price);
+    pavail.text("Product availability:  ");
+    avail_value.text(prods[n].availability);
+    total_value.text("Total price: " + tot);
+    show.append(pname);
+    show.append(name_value);
+    show.append(br1);
+    show.append(pprice);
+    show.append(price_value);
+    show.append(br2);
+    show.append(pavail);
+    show.append(avail_value);
+    show.append(br3);
+    show.append(total_value);
 }
 
 function Base(name, price) {
@@ -55,46 +55,56 @@ Product.prototype.removeCart = function (n) {
     } else {
         this.availability = this.availability + 1;
         tot = tot - this.price;
-        total_value.innerHTML = "Total price" + tot;
+        total_value.text("Total price" + tot);
     }
     display(n);
-    total_value.innerHTML = "Total price" + tot;
+    total_value.text("Total price" + tot);
 };
 
 Product.prototype.addCart = function (n) {
     "use strict";
     if (this.availability === 0) {
         display(n);
-        total_value.innerHTML = "Not available";
+        total_value.text("Not available");
     } else {
         this.availability = this.availability - 1;
         tot = tot + this.price;
-        total_value.innerHTML = "Total price" + tot;
+        total_value.text("Total price" + tot);
     }
     display(n);
-    total_value.innerHTML = "Total price" + tot;
+    total_value.text("Total price" + tot);
 };
     
 function start() {
     "use strict";
     var num = 0, count = 10, names = ["Shirts", "Accessories", "Sports", "Mobiles", "Books", "Computer", "Furniture", "Shoes", "Bags", "Wallet"], price = [100, 50, 200, 1000, 75, 2000, 3000, 150, 200, 50], no = [count, count, count, count, count, count, count, count, count, count], new_div, new_image;
-    while (num < 10) {
-        new_div = document.createElement("div");
-        new_div.setAttribute("class", "col-lg-3 col-md-4 col-xs-6 thumb loc");
-        new_div.setAttribute("id", num.toString());
-        document.getElementById("items").appendChild(new_div);
-        new_image = document.createElement("img");
-        new_image.setAttribute("class", "img-responsive items");
-        new_image.setAttribute("id", num.toString());
-        new_image.setAttribute("src", "/home/vishal/my_works/shopping cart/assets/" + num.toString() + ".jpg");
-        new_image.setAttribute("width", "100");
-        new_image.setAttribute("height", "100");
-        document.getElementById(num.toString()).appendChild(new_image);
+    while (num < 9) {
+        new_div = $("<div></div>");
+        new_div.addClass("col-lg-3 col-md-3 col-sm-2 col-xs-6 thumb limit");
+        new_div.attr("id", num.toString());
+        
+        if (num < 3) {
+            $("#items_1").append(new_div);
+        } else if (num >= 3 && num < 6) {
+            $("#items_2").append(new_div);
+        } else {
+            $("#items_3").append(new_div);
+        }
+        new_image = $("<img>");
+        new_image.addClass("img-responsive imgs");
+        new_image.attr("id", num.toString());
+        new_image.attr("src", "../shopping cart/assets/" + num.toString() + ".jpg");
+        new_image.attr("width", "100");
+        new_image.attr("height", "100");
+        $("#" + num.toString()).append(new_image);
         prods[num] = new Product(names[num], price[num], no[num]);
         num = num + 1;
     }
     
-    document.getElementById("add").addEventListener("click", function () {
+    new_div = $("<div></div>");
+    new_div.attr("id", "disp");
+    
+    $("#add").bind("click", function () {
         var product_name = name_value.innerHTML, n = 0;
         while (n < 10) {
             if (prods[n].name === product_name) {
@@ -105,7 +115,7 @@ function start() {
         }
     });
     
-    document.getElementById("cancel").addEventListener("click", function () {
+    $("#cancel").bind("click", function () {
         var product_name = name_value.innerHTML, n = 0;
         while (n < 10) {
             if (prods[n].name === product_name) {
@@ -117,11 +127,13 @@ function start() {
     });
     
     $(document).ready(function () {
-        
         $(".row").mouseenter(function () {
             $("img").mouseenter(function (e) {
                 var ele = e.currentTarget.id;
-                $("#" + ele).css("opacity", ".5");
+                $("#" + ele).css("opacity", "0.5");
+                /*$("#" + ele).animate({
+                    opacity : "0.5"
+                });*/
             });
             $("img").mouseout(function (e) {
                 var ele = e.currentTarget.id;
